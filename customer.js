52895ea0df7ac2ac07ba32390a3154832b927695
AdityaNaida <adityanaida@gmail.com>
2024-07-30 17:29:30 +0530

    [ADD] One to Many (Implementation)

---
+++ customer.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+const { Schema } = require("mongoose"); 
+
+main().then(() => {
+    console.log("Connected to DB");
+}).catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/relationDemo');
+}
+
+const orderSchema = new Schema({
+    item: String,
+    price: Number
+})
+
+const customerSchema = new Schema({
+    name: String,
+    orders: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Order" //the refrence is coming from where
+        }
+    ]
+})
+
+const Order = mongoose.model("Order", orderSchema);
+const Customer = mongoose.model("Customer", customerSchema);
+
+const findCustomer = async () => {
+    
+    let result = await Customer.find();
+
+    console.log(result);
+}
+
+findCustomer();
+
+// const addOrders = async () => {
+//     let res = await Order.insertMany([
+//         {
+//             item: "Samosa",
+//             price: 12
+//         },
+//         {
+//             item: "Protein Bar",
+//             price: 80
+//         },
+//         {
+//             item: "Creatine",
+//             price: 750
+//         }
+//     ])
+
+//     console.log(res)
+// }
+
+// addOrders();
+
+
